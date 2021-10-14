99d061f82cc2a64dc16c699eec8488b637b95744
Furkan <fportakal@protel.com.tr>
2021-10-14 21:28:11 +0300

    feat: style changes

--- components/event/card.js
+++ components/event/card.js
@@ -4,8 +4,10 @@ export default function Card({ title, desc, children }) {
   return (
     <article className={styles.wrapper}>
       {children}
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{desc}</p>
+      <div>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{desc}</p>
+      </div>
     </article>
   );
 }
